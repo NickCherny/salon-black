3a5f9d62908750e7467085450ff8b3249b71f02d
Nick Cherny <cherny.web@gmail.com>
2019-01-21 13:52:58 +0300

    fir eslint

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,38 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-
-exports.createPages = async ({
-  graphql, actions: { createPage }
-}) => {
-  const TargetComponent = require('./src/templates/catalog.js');
-  const { data, error } = await graphql`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            id
-            fileAbsolutePath:
-            frontmatter {
-              path
-            }
-          }
-        }
-      }
-    }
-  `;
-
-  if (error) {
-    console.error(error);
-  } else {
-    data.allMarkdownRemark.edges.forEach((edge) => {
-      createPage({
-        path: edge.node.fileAbsolutePath,
-        component: TargetComponent,
-        context: {
-          ...edge,
-        }
-      });
-    });
-  }
-};
